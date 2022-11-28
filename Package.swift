320c3f684a9b7bb33830df12d138e1dc62bcfe40
berkaypacal <pacalberkay@gmail.com>
2022-11-28 23:55:40 +0300

    initial commit

--- Package.swift
+++ Package.swift
@@ -21,8 +21,6 @@ let package = Package(
         .target(
             name: "NetworkManager",
             dependencies: []),
-        .testTarget(
-            name: "NetworkManagerTests",
-            dependencies: ["NetworkManager"]),
+        
     ]
 )
---
+++ Sources/NetworkManager/ManagerService.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Berkay Paçal on 11/28/22.
+//
+
+public struct ManagerService {
+    public var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func getString() -> String {
+        return text
+    }
+}
